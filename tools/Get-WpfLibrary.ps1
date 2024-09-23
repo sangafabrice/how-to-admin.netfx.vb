06c853f51c3dab30f6697559f6d23d46a8d79044
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-09-23 04:52:30 +0100

    feat: Initial wmi scripting source code

---
+++ tools/Get-WpfLibrary.ps1
@@ -0,0 +1,8 @@
+<#PSScriptInfo .VERSION 1.0.0#>
+
+[CmdletBinding()]
+param (
+  [string] $PartialName
+)
+
+return "$Env:windir\Microsoft.NET\Framework$(if ([Environment]::Is64BitOperatingSystem) { '64' })\v4.0.30319\WPF\$PartialName.dll"
