625eb73a889fd73f0a236259d0f26860b0545efd
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-11-15 19:56:19 +0100

    refactor: Replace WMI class with CLR types

--- src/AssemblyInfo.vb
+++ src/AssemblyInfo.vb
@@ -3,5 +3,6 @@ Imports System.Reflection
 <Assembly: AssemblyProduct("CvMd2Html Shortcut")>
 <Assembly: AssemblyCopyright("© 2024 sangafabrice")>
 <Assembly: AssemblyCompany("sangafabrice")>
-<Assembly: AssemblyInformationalVersion("0.0.1.445")>
-<Assembly: AssemblyVersion("0.0.1.445")>
+<Assembly: AssemblyTitle("CvMd2Html")>
+<Assembly: AssemblyInformationalVersion("0.0.1.572")>
+<Assembly: AssemblyVersion("0.0.1.572")>
