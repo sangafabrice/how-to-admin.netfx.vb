bb6bd775848c3b56a319dba83311d9ebe08440b8
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-11-15 19:56:19 +0100

    refactor: Replace WMI class with CLR types

--- package.ps1
+++ package.ps1
@@ -1,4 +1,4 @@
-<#PSScriptInfo .VERSION 1.0.3#>
+<#PSScriptInfo .VERSION 1.0.4#>
 
 using namespace System.Management.Automation
 [CmdletBinding()]
@@ -7,28 +7,6 @@ Param ()
 & {
   Remove-Item ($LibDir = "$PSScriptRoot\lib") -Recurse -ErrorAction SilentlyContinue
   [void] (New-Item $LibDir -ItemType Directory -ErrorAction Stop)
-
-  Function ImportMgmtClass {
-    <#
-    .DESCRIPTION
-    This function compiles a Management Class' source code to dll libraries.
-    .NOTES
-    It must be called after getting the library Interop.IWshRuntimeLibrary.dll.
-    It must also be called after the vbc.exe path is added to PATH environment variable.
-    <Class_Name> means the name of the class may have an underscore '_'
-    which is replaced with '.' in the source code base name <Class.Name>.
-    .PARAMETER ClassName
-    The specified Management class name.
-    #>
-    Param (
-      [string] $ClassName
-    )
-    $EnvPath = $Env:Path
-    $Env:Path = "$Env:windir\Microsoft.NET\Framework$(If ([Environment]::Is64BitOperatingSystem) { '64' })\v4.0.30319\;$Env:Path"
-    $FileName = $ClassName.Replace('_', '.')
-    vbc.exe /nologo /target:library /out:"$LibDir\$FileName.dll" "$(($SrcDir = "$PSScriptRoot\src"))\AssemblyInfo.vb" "$SrcDir\$FileName.vb"
-    $Env:Path = $EnvPath
-  }
 
   Function ImportTypeLibrary {
     <#
@@ -49,7 +27,6 @@ Param ()
   }
 
   ImportTypeLibrary 'C:\Windows\System32\wshom.ocx' 'IWshRuntimeLibrary'
-  ImportMgmtClass StdRegProv
 
   Get-ChildItem "$LibDir\*" -Directory | Remove-Item -Recurse -Force
   'PresentationFramework','WindowsBase','PresentationCore' |
