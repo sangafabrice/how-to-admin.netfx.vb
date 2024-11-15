619a0b3024ca6187cd379e14286fe6bcc2af08a3
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2024-11-15 19:56:19 +0100

    refactor: Replace WMI class with CLR types

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-<#PSScriptInfo .VERSION 1.0.0#>
+<#PSScriptInfo .VERSION 1.0.1#>
 
 using namespace System.Management.Automation
 [CmdletBinding()]
@@ -23,10 +23,11 @@ param ()
   New-Item $BinDir -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
   Copy-Item "$PSScriptRoot\rsc" -Destination $BinDir -Recurse
   Save-ProjectPackage $PSScriptRoot
+  Set-ProjectVersion $PSScriptRoot
 
   # Compile the source code with vbc.exe.
   $vbc_dll = dotnet.exe --list-sdks | Select-Object -Last 1 | ForEach-Object { $_ -replace '([\d\.]+) \[(.+)\]','$2\$1\Roslyn\bincore\vbc.dll' }
-  dotnet.exe $vbc_dll /nologo /target:$($DebugPreference -eq 'Continue' ? 'exe':'winexe') /win32icon:"$PSScriptRoot\menu.ico" /imports:Microsoft.VisualBasic,System,System.Linq /reference:"$BinDir\Interop.IWshRuntimeLibrary.dll" /reference:$(Get-WpfLibrary PresentationFramework) /reference:$(Get-WpfLibrary PresentationCore) /reference:$(Get-WpfLibrary WindowsBase) /reference:$(Get-CoreLibrary System.Xaml) /reference:$(Get-CoreLibrary System.Management) /reference:$(Get-CoreLibrary System.Text.RegularExpressions) /reference:$(Get-CoreLibrary System.Linq) /reference:$(Get-CoreLibrary System.Core) /reference:$(Get-CoreLibrary System) /reference:$(Get-CoreLibrary mscorlib) /vbruntime:$(Get-CoreLibrary Microsoft.VisualBasic) /out:$(($ConvertExe = "$BinDir\cvmd2html.exe")) "$(($SrcDir = "$PSScriptRoot\src"))\AssemblyInfo.vb" "$SrcDir\ErrorLog.vb" "$SrcDir\Package.vb" "$SrcDir\Parameters.vb" "$PSScriptRoot\Program.vb" "$SrcDir\Setup.vb" "$SrcDir\Util.vb"
+  dotnet.exe $vbc_dll /nologo /target:$($DebugPreference -eq 'Continue' ? 'exe':'winexe') /win32icon:"$PSScriptRoot\menu.ico" /win32manifest:"$PSScriptRoot\cvmd2html.manifest" /imports:Microsoft.VisualBasic,System,System.Linq /reference:"$BinDir\Interop.IWshRuntimeLibrary.dll" /reference:$(Get-WpfLibrary PresentationFramework) /reference:$(Get-WpfLibrary PresentationCore) /reference:$(Get-WpfLibrary WindowsBase) /reference:$(Get-CoreLibrary System.Xaml) /reference:$(Get-CoreLibrary System.Management) /reference:$(Get-CoreLibrary System.Text.RegularExpressions) /reference:$(Get-CoreLibrary System.Linq) /reference:$(Get-CoreLibrary System.Core) /reference:$(Get-CoreLibrary System) /reference:$(Get-CoreLibrary mscorlib) /vbruntime:$(Get-CoreLibrary Microsoft.VisualBasic) /out:$(($ConvertExe = "$BinDir\cvmd2html.exe")) "$(($SrcDir = "$PSScriptRoot\src"))\AssemblyInfo.vb" "$SrcDir\ErrorLog.vb" "$SrcDir\Package.vb" "$SrcDir\Parameters.vb" "$PSScriptRoot\Program.vb" "$SrcDir\Setup.vb" "$SrcDir\Util.vb"
 
   if ($LASTEXITCODE -eq 0) {
     Write-Host "Output file $ConvertExe written." @HostColorArgs -NoNewline
